d5e9fb37dbb562d2849463074b8e37b7be02139f
Leo Lanese <developer@leolanese.com>
2018-08-09 20:59:11 +0100

    Update order2.js

--- nodejs-project/pure/order2.js
+++ nodejs-project/pure/order2.js
@@ -1,10 +1,11 @@
+// shared-state: 2 -> 4 -> 5
 const x = {
   val: 2
 };
 const x1 = () => x.val += 1;
 const x2 = () => x.val *= 2;
 // ...the order of the function calls is reversed...
-x2();
-x1();
+x2(); // 2 * 2 = 4
+x1(); // 4 + 1 = 5
 // which changes the resulting value:
 console.log(x.val); // 5
