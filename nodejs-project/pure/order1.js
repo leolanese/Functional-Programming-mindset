66cd3b4dbb16cd25a8c4fb33ab5e76789e5fff40
Leo Lanese <developer@leolanese.com>
2018-08-09 20:57:32 +0100

    Update order1.js

--- nodejs-project/pure/order1.js
+++ nodejs-project/pure/order1.js
@@ -1,8 +1,8 @@
 const x = {
-  val: 2
+  val: 2; // shared-state: 2 -> 3 -> 6
 };
 const x1 = () => x.val += 1;
 const x2 = () => x.val *= 2;
-x1();
-x2();
+x1(); // 2 + 1 = 3
+x2(); // 3 * 2 = 6 
 console.log(x.val); // 6
